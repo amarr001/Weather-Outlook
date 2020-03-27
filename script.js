dc394ed273d3a1bc5a8e1d9ac2e59b2940ba047c
Ana Marr <acarolinasim@gmail.com>
2020-03-28 10:10:15 +1030

    updates

--- script.js
+++ script.js
@@ -14,7 +14,16 @@ event.preventDefault();
         cities.push(input);
         cityInput.val("");
 
-        displayCity();
+        var list = $("<li>")
+        var cityBtn = $("<button>").attr({"class": "btn btn-secondary cityBtn"})
+        cityBtn.text(input);
+        deleteBtn = $("<button>").attr({"class": "btn btn-info deleteBtn"});
+        deleteIcon = $("<i>").attr({"class": "far fa-trash-alt"});
+        deleteBtn.append(deleteIcon);
+    
+        cityList.append(list);
+        cityBtn.append(deleteBtn);
+        list.append(cityBtn);
  
    
     localStorage.setItem("cities", JSON.stringify(cities));
@@ -51,22 +60,12 @@ function renderCities(){
 
 
 }
-function displayCity(){
-    var list = $("<li>")
-    var cityBtn = $("<button>").attr({"class": "btn btn-secondary cityBtn"})
-    cityBtn.text(input);
-    deleteBtn = $("<button>").attr({"class": "btn btn-info deleteBtn"});
-    deleteIcon = $("<i>").attr({"class": "far fa-trash-alt"});
-    deleteBtn.append(deleteIcon);
-
-    cityList.append(list);
-    cityBtn.append(deleteBtn);
-    list.append(cityBtn);
-}
 
 $(".deleteBtn").on("click", function(){
 
     $(this).parent().remove();
     
     console.log(cities);
+
+    localStorage.setItem("cities", JSON.stringify(cities));
 })
