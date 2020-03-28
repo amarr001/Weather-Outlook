17f499f98fc9d2cce8e4fe1560162e40ddaa0914
Ana Marr <acarolinasim@gmail.com>
2020-03-28 20:53:14 +1030

    updates-icon

--- script.js
+++ script.js
@@ -1,71 +1,102 @@
 var cityList = $("#cityList");
 var cityInput = $(".cityInput");
-var input = cityInput.val();
-var cityBtn; 
 var cities = [];
+var weatherDisplay = $(".weatherDisplay");
+var currentDate = moment().format('dddd, MMMM Do');
+
+
 
 cityList.empty();
-init();
 
 $("#searchCity").on("click", function(event){
 event.preventDefault();
 
         var input = cityInput.val();
         cities.push(input);
+        if(input === ""){
+            return;
+        }
+        
         cityInput.val("");
+    
 
         var list = $("<li>")
         var cityBtn = $("<button>").attr({"class": "btn btn-secondary cityBtn"})
         cityBtn.text(input);
-        deleteBtn = $("<button>").attr({"class": "btn btn-info deleteBtn"});
-        deleteIcon = $("<i>").attr({"class": "far fa-trash-alt"});
-        deleteBtn.append(deleteIcon);
     
         cityList.append(list);
-        cityBtn.append(deleteBtn);
         list.append(cityBtn);
- 
-   
-    localStorage.setItem("cities", JSON.stringify(cities));
 
-    console.log(cities);
+        /*for(i=0; i<cities.length; i++){
 
-});
+            var city = [{
+                cityId: $(".cityBtn").attr("data-city-id"),
+                cityName: $(".cityBtn").attr("data-city-name"), // GET THE NAME OF THE CITY
+                cityTemp: $(".cityBtn").attr("data-city-temp"),// GET THE TEMP OF THE CITY
+                cityUVIndex: $(".cityBtn").attr("data-city-cityUVIndex") // GET THE cityUVIndex OF THE CITY
+            }];
+            
+            cities[i] = city
 
-function init(){
+            if(input === "Adelaide"){
+                city.cityId = "7839644"
+            
+            }
 
-    var storedCity = JSON.parse(localStorage.getItem("cities"));
-    if (cities !== null){
-        cities = storedCity
-    }
+            if(input === "Melbourne"){
+                city.cityId = "7839805"
+                
+            }
+            if(input === "Brisbane"){
+                city.cityId = "2174003"
+                
+            }
+            if(input === "Sydney"){
+                cityId = "6619279"
+                
+            }
+            if(input === "Darwin"){
+                cityId = "7839402"
+                
+            }
+            if(input === "Hobart"){
+                cityId = "2163355"
+                
+            }
+            
+        }*/
 
-renderCities();
+        console.log(cities);
+        console.log(cityBtn);
 
-}
+    cityBtn.on("click", function(event){
+        event.preventDefault();
 
-function renderCities(){
+        if(input === "Adelaide"){
 
-    for(i=0; i<cities.length; i++){
+        var APIkey = "c876a593b2fe39ad3d8e6cb1bc1dd14d";
 
-        var list = $("<li>")
-        var cityBtn = $("<button>").attr({"class": "btn btn-secondary cityBtn"})
-        cityBtn.text(cities[i]);
-        deleteBtn = $("<button>").attr({"class": "btn btn-info deleteBtn"});
-        deleteIcon = $("<i>").attr({"class": "far fa-trash-alt"});
-        deleteBtn.append(deleteIcon);
-        cityBtn.append(deleteBtn);
-        cityList.append(list);
-        list.append(cityBtn);
-    }
+                 var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+                "id=7839644&appid=" + APIkey;
+       
+                $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            // We store all of the retrieved data inside of an object called "response"
+            .then(function(response) {
 
+                console.log(queryURL);
+                console.log(response);
 
-}
+                var iconCode = response.weather[0].icon;
+                var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+                
+               $(".cityName").html("<h1>" + response.name + "<img src='" + iconUrl  + "'>" + "</h1>" + "<h4>" + currentDate + "</h4>" );
 
-$(".deleteBtn").on("click", function(){
+        
+            })
+        }
 
-    $(this).parent().remove();
-    
-    console.log(cities);
-
-    localStorage.setItem("cities", JSON.stringify(cities));
-})
+        })
+    });
